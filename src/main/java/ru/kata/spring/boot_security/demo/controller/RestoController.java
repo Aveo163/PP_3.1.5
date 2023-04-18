ef3a9adfb01e36188b6b92354a8bbc950cfa30a1
aveo163 <andruharuss@mail.ru>
2023-04-18 13:51:14 +0400

    commit

--- src/main/java/ru/kata/spring/boot_security/demo/controller/RestoController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/RestoController.java
@@ -4,26 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@org.springframework.web.bind.annotation.RestController
+@RestController
 @RequestMapping("/api")
-public class RestController {
+public class RestoController {
     private final UserService userService;
 
     @Autowired
-    public RestController(UserService userService) {
+    public RestoController(UserService userService) {
         this.userService = userService;
     }
 
